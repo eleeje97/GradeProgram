4aeb59cea0d9c5b5a69b5a08d42fa04af0784f33
kwonsye <kwonsye@naver.com>
2018-11-21 11:29:55 +0900

    add Student.java and StudentDatabase.java and read students_info file

---
+++ GradeProject/src/Student.java
@@ -0,0 +1,48 @@
+//학생 클래스
+public class Student {
+	
+	//학생 정보
+	private String studentID; //학번
+	private String name; //이름
+	private String phoneNumber; //전화번호
+	
+	//학생 성적
+	public int koreaGrade; //국어 성적
+	public int mathGrade; //수학성적
+	public int englishGrade; //영어성적
+	public int societyGrade; //사회성적
+	public int scienceGrade; //과학성적
+	
+	//생성자
+	public Student(String studentID, String name, String phoneNumber) {
+		
+		this.studentID = studentID;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		
+	}
+	
+	public String getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(String studentID) {
+		this.studentID = studentID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
---
+++ GradeProject/src/StudentsDatabase.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Vector;
+
+//학생데이터베이스를 가지고 있는 클래스
+public class StudentsDatabase {
+	
+	//학생객체들이 저장되어있는 학생 데이터베이스 벡터
+	private static Vector<Student> studentsDatabase = new Vector<>();
+	
+	//학생 데이터베이스는 싱글턴패턴을 적용
+	public Vector<Student> getStudentsDatabase() {
+		return studentsDatabase;
+	}
+	
+	//student_info.txt 파일을 읽어서 학생 데이터베이스에 저장하는 메소드
+	public static void readStudentsInfoFile(String fileName) throws IOException{
+		File studentsInfoFile = new File(fileName);
+		Scanner scanner = new Scanner(studentsInfoFile);
+		
+		while(scanner.hasNext()) {
+			String studentID = scanner.next();
+			String name = scanner.next();
+			String phoneNumber = scanner.next();
+			
+			Student student = new Student(studentID, name, phoneNumber);
+			//학생 데이터베이스에 저장
+			studentsDatabase.add(student);
+		}
+		
+	}
+	
+	/*test case
+	//학생정보파일 읽기(초기 한번만 실행)
+	public static void main(String[] args) throws IOException {
+		
+		studentsDatabase.clear(); //기존 디비 초기화
+		
+		readStudentsInfoFile("students_info.txt");
+		
+		for(Student s : studentsDatabase) {
+			System.out.print(s.getName()+"/"+s.getStudentID()+"/"+s.getPhoneNumber());
+			System.out.println("");
+		}
+	}
+	 */
+}
