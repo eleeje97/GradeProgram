e8600f5e2c540b71195ee6cbe1da89cf088d0abc
eleeje97 <eleeje97@gmail.com>
2018-11-22 18:55:05 +0900

    for backup

--- GradeProject/src/studentPackage/StudentsDatabase.java
+++ GradeProject/src/studentPackage/StudentsDatabase.java
@@ -7,17 +7,29 @@ import java.util.Vector;
 
 //학생데이터베이스를 가지고 있는 클래스
 public class StudentsDatabase {
+	//자기자신을 속성으로 가짐
+	private static StudentsDatabase singleton = new StudentsDatabase();
 	
 	//학생객체들이 저장되어있는 학생 데이터베이스 벡터
-	private static Vector<Student> studentsDatabase = new Vector<>();
+	private Vector<Student> studentsDatabase;
 	
 	//학생 데이터베이스는 싱글턴패턴을 적용
-	public Vector<Student> getStudentsDatabase() {
-		return studentsDatabase;
+	public static Vector<Student> getStudentsDatabase() {
+		return singleton.studentsDatabase;
+	}
+	
+	private StudentsDatabase(){
+		try {
+			this.readStudentsInfoFile("students_info.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	//student_info.txt 파일을 읽어서 학생 데이터베이스에 저장하는 메소드
-	public static void readStudentsInfoFile(String fileName) throws IOException{
+	public void readStudentsInfoFile(String fileName) throws IOException{
+		studentsDatabase = new Vector<>();
+		
 		File studentsInfoFile = new File(fileName);
 		Scanner scanner = new Scanner(studentsInfoFile);
 		
@@ -28,23 +40,23 @@ public class StudentsDatabase {
 			
 			Student student = new Student(studentID, name, phoneNumber);
 			//학생 데이터베이스에 저장
-			studentsDatabase.add(student);
+			singleton.studentsDatabase.add(student);
 		}
-		
+		scanner.close();
 	}
 	
-	/*test case
+	//test case
 	//학생정보파일 읽기(초기 한번만 실행)
 	public static void main(String[] args) throws IOException {
 		
-		studentsDatabase.clear(); //기존 디비 초기화
+		//singleton.studentsDatabase.clear(); //기존 디비 초기화
 		
-		readStudentsInfoFile("students_info.txt");
+		//singleton.readStudentsInfoFile("students_info.txt");
 		
-		for(Student s : studentsDatabase) {
+		for(Student s : singleton.studentsDatabase) {
 			System.out.print(s.getName()+"/"+s.getStudentID()+"/"+s.getPhoneNumber());
 			System.out.println("");
 		}
 	}
-	 */
+	
 }
